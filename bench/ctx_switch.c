18326d0175b81bdda069a9a7a0e0c273cbd896b7
Jeffrey Helt <jh1@fastmail.com>
2018-07-27 15:39:20 -0400

    fixes

--- bench/ctx_switch.c
+++ bench/ctx_switch.c
@@ -10,15 +10,11 @@
 
 #define BUF_LEN 1
 
-struct targ {
+struct thread_arg {
+	int niters;
 	int rfd;
 	int wfd;
 };
-
-void sighandler(int signum)
-{
-	pthread_exit(EXIT_SUCCESS);
-}
 
 void handle_error(char *msg)
 {
@@ -28,13 +24,16 @@ void handle_error(char *msg)
 
 void *thread_main(void *arg)
 {
-	struct targ *targ = (struct targ*)arg;
-	ssize_t n;
+	struct thread_arg *targ = (struct thread_arg*)arg;
+
+	char buf[BUF_LEN] = {1};
+	int i;
+	int niters = targ->niters;
 	int rfd = targ->rfd;
 	int wfd = targ->wfd;
-	char buf[BUF_LEN];
+	ssize_t n;
 
-	while (true) {
+	for (i = 0; i < niters; i++) {
 		n = read(rfd, buf, BUF_LEN);
 		if (n != BUF_LEN)
 			handle_error("thread read");
@@ -47,16 +46,11 @@ void *thread_main(void *arg)
 	return EXIT_SUCCESS;
 }
 
-void init_thrs(int n, struct targ *args, pthread_t *thrs)
+void init_threads(int n, struct thread_arg *args, pthread_t *threads)
 {
-	int i, ret;
+	int i;
+	int ret;
 	pthread_attr_t attr;
-	struct sigaction sa;
-
-	sa.sa_handler = sighandler;
-	ret = sigaction(SIGUSR1, &sa, NULL);
-	if (ret)
-		handle_error("sigaction");
 
 	ret = pthread_attr_init(&attr);
 	if (ret != 0) {
@@ -65,7 +59,7 @@ void init_thrs(int n, struct targ *args, pthread_t *thrs)
 	}
 
 	for (i = 0; i < n; i++) {
-		ret = pthread_create(&thrs[i], &attr, &thread_main, &args[i]);
+		ret = pthread_create(&threads[i], &attr, &thread_main, &args[i]);
 		if (ret != 0) {
 			errno = ret;
 			handle_error("pthread_create");
@@ -79,18 +73,13 @@ void init_thrs(int n, struct targ *args, pthread_t *thrs)
 	}
 }
 
-void destroy_thrs(int n, pthread_t *thrs)
+void destroy_threads(int n, pthread_t *threads)
 {
-	int i, ret;
+	int i;
+	int ret;
 
 	for (i = 0; i < n; i++) {
-		ret = pthread_kill(thrs[i], SIGUSR1);
-		if (ret != 0) {
-			errno = ret;
-			handle_error("pthread_kill");
-		}
-
-		ret = pthread_join(thrs[i], NULL);
+		ret = pthread_join(threads[i], NULL);
 		if (ret != 0) {
 			errno = ret;
 			handle_error("pthread_join");
@@ -98,50 +87,59 @@ void destroy_thrs(int n, pthread_t *thrs)
 	}
 }
 
-void init_args(int n, struct targ *args)
+void init_args(int nswitch, int nthreads, int nwarmups,
+	struct thread_arg *args)
 {
 	int i;
+	int niters = ((nswitch + (nthreads - 1)) / nthreads) + nwarmups;
 	int pipefd[2];
 
-	for (i = 0; i < n; i++) {
+	for (i = 0; i < nthreads; i++) {
 		if (pipe(pipefd) != 0)
 			handle_error("pipe");
 
+		args[i].niters = niters;
 		args[i].wfd = pipefd[1];
-		args[(i+1) % n].rfd = pipefd[0];
+		args[(i+1) % nthreads].rfd = pipefd[0];
 	}
 }
 
-void destroy_args(int n, struct targ *args)
+void destroy_args(int n, struct thread_arg *args)
 {
 	int i;
+
 	for (i = 0; i < n; i++) {
 		close(args[i].rfd);
 		close(args[i].wfd);
 	}
 }
 
-void warmup(int rfd, int wfd)
+void warmup(int rfd, int wfd, int nwarmups)
 {
-	char buf[BUF_LEN];
-	if (write(wfd, buf, BUF_LEN) != BUF_LEN)
-		handle_error("write");
+	char buf[BUF_LEN] = {1};
+	int i;
 
-	if (read(rfd, buf, BUF_LEN) != BUF_LEN)
-		handle_error("read");
+	for (i = 0; i < nwarmups; i++) {
+		if (write(wfd, buf, BUF_LEN) != BUF_LEN)
+			handle_error("write");
+
+		if (read(rfd, buf, BUF_LEN) != BUF_LEN)
+			handle_error("read");
+	}
 }
 
-void run_bench(int rfd, int wfd, int n_ctx, int n_thrs)
+void run_bench(int rfd, int wfd, int nwarmups, int nswitch, int nthreads)
 {
-	char buf[BUF_LEN];
+	char buf[BUF_LEN] = {1};
 	int i;
-	struct timespec start, end;
+	struct timespec end;
+	struct timespec start;
 	uint64_t delta;
 
-	warmup(rfd, wfd);
+	warmup(rfd, wfd, nwarmups);
 
 	clock_gettime(CLOCK_REALTIME, &start);
-	for (i = 0; i < n_ctx; i += n_thrs) {
+	for (i = 0; i < nswitch; i += nthreads) {
 		if (write(wfd, buf, BUF_LEN) != BUF_LEN)
 			handle_error("write");
 
@@ -153,32 +151,65 @@ void run_bench(int rfd, int wfd, int n_ctx, int n_thrs)
 	delta = 1e9 * (end.tv_sec - start.tv_sec)
 		+ end.tv_nsec - start.tv_nsec;
 
-	printf("n_ctx = %d, delta = %ldns, avg=%ldns\n", i, delta, delta/i);
+	printf("nswitch = %d, delta = %ldns, avg = %ldns\n", i, delta, delta/i);
 }
 
-int main(int argc, char *argv)
+void print_usage(char *name)
 {
-	int n_ctx = 1000000, n_thrs = 3;
-	struct targ *args;
-	pthread_t *thrs;
+	fprintf(stderr, "Usage: %s [-w nwarmups] -n nswitches -t nthreads\n",
+		name);
 
-	// Parse n_ctx and n_thrs
+	exit(EXIT_FAILURE);
+}
 
-	args = calloc(n_thrs, sizeof(struct targ));
+int main(int argc, char **argv)
+{
+	char c;
+	int nswitch = -1;
+	int nthreads = -1;
+	int nwarmups = 1;
+	pthread_t *threads;
+	struct thread_arg *args;
+
+	opterr = 0;
+
+	while ((c = getopt(argc, argv, "w:n:t:")) != -1)
+		switch (c) {
+		case 'n':
+			nswitch = atoi(optarg);
+			break;
+		case 't':
+			nthreads = atoi(optarg);
+			break;
+		case 'w':
+			nwarmups = atoi(optarg);
+			break;
+		default:
+			print_usage(argv[0]);
+		}
+
+
+	if (nswitch == -1 || nthreads == -1)
+		print_usage(argv[0]);
+
+	args = calloc((size_t)nthreads, sizeof(struct thread_arg));
 	if (args == NULL)
 		handle_error("calloc");
 
-	thrs = calloc(n_thrs - 1, sizeof(pthread_t));
-	if (thrs == NULL)
+	threads = calloc((size_t)nthreads - 1, sizeof(pthread_t));
+	if (threads == NULL)
 		handle_error("calloc");
 
-	init_args(n_thrs, args);
-	init_thrs(n_thrs - 1, &args[1], thrs); // Thread 0 == main
+	init_args(nswitch, nthreads, nwarmups, args);
+	init_threads(nthreads - 1, &args[1], threads); // Thread 0 == main
 
-	run_bench(args[0].rfd, args[0].wfd, n_ctx, n_thrs);
+	run_bench(args[0].rfd, args[0].wfd, nwarmups, nswitch, nthreads);
 
-	destroy_thrs(n_thrs - 1, thrs);
-	destroy_args(n_thrs, args);
+	destroy_threads(nthreads - 1, threads);
+	destroy_args(nthreads, args);
+
+	free(threads);
+	free(args);
 
 	return EXIT_SUCCESS;
 }
